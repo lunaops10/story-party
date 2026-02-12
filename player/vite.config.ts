ec1483e19c899ab543b2cc7c52dccf893b590bcd
Luna Assistant <luna@Lunas-MacBook-Air.local>
2026-02-12 08:48:43 +0100

    Fix env var handling

--- player/vite.config.ts
+++ player/vite.config.ts
@@ -3,9 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "import.meta.env.VITE_SERVER_URL": JSON.stringify(
-      process.env.VITE_SERVER_URL || "ws://localhost:2567"
-    ),
-  },
 });
